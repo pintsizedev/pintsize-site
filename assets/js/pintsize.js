b63a992770af928b9237cc88377bbd0e38f1f4de
pintsizedev <dandev@pintsize.it>
2014-11-20 19:32:58 +0000

    started home page

---
+++ assets/js/pintsize.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('#introButton').click(function() {
+       $('html, body').animate({
+           scrollTop: $("#about").offset().top
+       }, 2000);
+    });
+});
