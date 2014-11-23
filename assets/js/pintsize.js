e64bb281ad60981b27447dc3d3bafd954270e9bc
pintsizedev <dandev@pintsize.it>
2014-11-23 21:29:12 +0000

    Added sidebar with some functionality, exciting

--- assets/js/pintsize.js
+++ assets/js/pintsize.js
@@ -1,11 +1,36 @@
 $(document).ready(function(){
+    var scrollTime = 2000;
     $('#introButton').click(function() {
-       $('html, body').animate({
-           scrollTop: $("#about").offset().top
-       }, 2000);
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, scrollTime);
+        return false;
+    });
+
+    $('#side-about').click(function() {
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, scrollTime);
+        return false;
+    });
+
+    $('#side-what-i-do').click(function() {
+        $('html, body').animate({
+            scrollTop: $("#what-i-do").offset().top
+        }, scrollTime);
+        return false;
     });
 
     $('.tab-container').organicTabs();
 
+    $('#sidenav-open').click(function() {
+        $('#sidenav-content').addClass('sidenav-open');
+        return false;
+    });
+
+    $('#sidenav-close').click(function() {
+        $('#sidenav-content').removeClass('sidenav-open');
+        return false;
+    });
 
 });
